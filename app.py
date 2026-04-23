30ff99557c085cb05c682a648612ecb2604355c0
Pallas13 <edsondasilvaguedes0@gmail.com>
2026-04-23 09:13:14 -0300

    Comentário a respeito do uso da função do restaurador

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+# Importa função de restaurar pontuação do moto
 from src.punt_restore import restaurar_pontuacao
 
+# Uso atual:
+# A função restaurar_pontuacao é necessário fornecer um texto ou string.
+# O retorno no momento será uma string, futuramente com o motor do restaurado funcionando a função irá funcionar corretamente.
 resultado = restaurar_pontuacao("oi")
 
 print(resultado)
