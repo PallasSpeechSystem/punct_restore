f75196064b053f392f3026cd35a2f0fc93d6e947
Pallas13 <edsondasilvaguedes0@gmail.com>
2026-05-03 14:07:17 -0300

    Atualização sobre o uso da função restaurar_pontuacao Adição de uso geral sobre a função restaurar_pontuacao, informando o tipo de entrada e saída.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # Importa função de restaurar pontuação do moto
 from src.punt_restore import restaurar_pontuacao
 
-# Uso atual:
-# A função restaurar_pontuacao é necessário fornecer um texto ou string.
-# O retorno no momento será uma string, futuramente com o motor do restaurado funcionando a função irá funcionar corretamente.
+# Uso Geral:
+# A função restaurar_pontuacao é necessário fornecer um texto ou uma string.
+# A função retornará uma string com texto pontuado como resposta.
+# Caso, ocorra erros a função restaurar_pontuacao retornará qual erro é
 resultado = restaurar_pontuacao("oi")
 
 print(resultado)
